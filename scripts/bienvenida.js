08ac186332b883cc49883de1db5af6f12fe9f453
johnlennonl <jojopow2410@gmail.com>
2024-08-25 19:22:24 -0400

    Bienvenida

--- scripts/bienvenida.js
+++ scripts/bienvenida.js
@@ -2,17 +2,21 @@
 function mostrarMensajeBienvenida() {
     Swal.fire({
       title: '¡Bienvenidos!',
-      imageUrl: 'https://github.com/johnlennonl/BitshopVE/blob/main/img/bitshop%20(8).png?raw=true', // Reemplaza con la ruta completa a tu imagen
-      imageWidth: 170, // Ajusta el ancho de la imagen según sea necesario
+      imageUrl: 'img/logoBlack.png', // Reemplaza con la ruta completa a tu imagen
+      imageWidth: 230, // Ajusta el ancho de la imagen según sea necesario
       imageHeight: 170, // Ajusta la altura de la imagen según sea necesario
       imageAlt: 'Imagen del producto',
       text: '¡Estás en el lugar correcto! En Bitshop Venezuela encontrarás prendas de calidad y diseño para cualquier ocasión. ¡Renueva tu armario hoy mismo!',
-      
       confirmButtonText: '¡Empezar a explorar!',
       confirmButtonColor: '#111',
       textColor: '#111',
       textFontSize: '16px',
       textFontFamily: 'Poppins',
+      customClass: {
+        popup: 'custom-swal',
+        confirmButton: 'custom-swal-button',
+        title: 'custom-swal-text'
+      }
     });
   
     // Almacenar en localStorage para no mostrar el mensaje nuevamente
